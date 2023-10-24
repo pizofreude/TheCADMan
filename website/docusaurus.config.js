dd80c4a81f6c25ba1a8934aba7339366e2289a57
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-10-24 17:17:16 +0800

    feat: add copyright

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -127,7 +127,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} TheCADMan. Built with Docusaurus. Maintained by <a href="https://github.com/pizofreude/TheCADMan">Pizofreude</a>.`,
       },
       prism: {
         theme: lightCodeTheme,
