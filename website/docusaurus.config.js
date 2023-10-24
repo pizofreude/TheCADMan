9f9155354812b62fb05cbd55f18cf46cd06878cc
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-10-24 17:03:51 +0800

    feat: update navbar title

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'TheCADMan',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
