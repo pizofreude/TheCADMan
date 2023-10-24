331e7a2392958c40bb122afcd195378c5ea682ee
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-10-24 17:02:33 +0800

    feat: update blog editUrl

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -49,7 +49,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/pizofreude/TheCADMan/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
