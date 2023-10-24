74eaa64bd4af418e93622560310f22da7b3a3fd9
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-10-25 00:47:45 +0800

    feat: update url for gh pages

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: '/TheCADMan/',
+  url: 'https://pizofreude.github.io',
   // url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
